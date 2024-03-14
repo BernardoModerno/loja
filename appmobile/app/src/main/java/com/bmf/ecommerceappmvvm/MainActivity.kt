5e872f41551f44144d4df32c448826662cf4136a
BernardoModerno <bernardo.moderno@gmail.com>
2024-03-14 19:18:06 -0300

    Desenho tela de Login Parte 1

--- appmobile/app/src/main/java/com/bmf/ecommerceappmvvm/MainActivity.kt
+++ appmobile/app/src/main/java/com/bmf/ecommerceappmvvm/MainActivity.kt
@@ -3,13 +3,24 @@ package com.bmf.ecommerceappmvvm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bmf.ecommerceappmvvm.ui.theme.EcommerceAppMVVMTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +30,47 @@ class MainActivity : ComponentActivity() {
             EcommerceAppMVVMTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    LoginContent()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun LoginContent() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Imagem de fundo",
+            contentScale = ContentScale.Crop,
+            colorFilter = ColorFilter.colorMatrix(
+                ColorMatrix().apply {
+                    setToScale(0.5f, 0.5f, 0.5f, 1f)
+                }
+            )
+        )
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(top = 50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(100.dp),
+                painter = painterResource(id = R.drawable.shopping_cart_blue),
+                contentDescription = "Logo"
+            )
+            Text(
+                modifier = Modifier.padding(top = 7.dp),
+                text = "ECOMMERCE APP",
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                fontSize = 20.sp,
+            )
+        }
+        
     }
 }
 
